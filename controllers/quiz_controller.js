c7cf9ba2b1587b5b33ac7f897a80cc5a70833f6c
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2015-08-01 14:28:20 -0300

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,25 +1,36 @@
 var models = require('../models/models.js');
 
 // GET /quizes/question
-exports.question = function (req, res) {
-    models.Quiz.findAll().success(function (quiz) {
-        res.render('quizes/question', {
-            pregunta: quiz[0].pregunta
+exports.show = function (req, res) {
+    models.Quiz.find(req.params.quizId).then(function (quiz) {
+        res.render('quizes/show', {
+            quiz: quiz
         });
     })
 };
 
 // GET /quizes/answer
 exports.answer = function (req, res) {
-    models.Quiz.findAll().success(function (quiz) {
-        if (req.query.respuesta === quiz[0].respuesta) {
+    models.Quiz.find(req.params.quizId).then(function (quiz) {
+        if (req.query.respuesta === quiz.respuesta) {
             res.render('quizes/answer', {
+                quiz: quiz,
                 respuesta: 'Correcto'
             });
         } else {
             res.render('quizes/answer', {
+                quiz: quiz,
                 respuesta: 'Incorrecto'
             });
         }
+    })
+};
+
+// GET /quizes
+exports.index = function (req, res) {
+    models.Quiz.findAll().then(function (quizes) {
+        res.render('quizes/index.ejs', {
+            quizes: quizes
+        });
     })
 };
--- models/models.js
+++ models/models.js
@@ -3,13 +3,13 @@ var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name = (url[6]||null);
-var user = (url[2]||null);
-var pwd = (url[3]||null);
-var protocol = (url[1]||null);
-var dialect = (url[1]||null);
-var port = (url[5]||null);
-var host = (url[4]||null);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
 var storage = process.env.DATABASE_STORAGE;
 
 // Cargar modulo ORM
@@ -21,8 +21,8 @@ var sequelize = new Sequelize(DB_name, user, pwd, {
     protocol: protocol,
     port: port,
     host: host,
-    storage: storage,   // solo SQLite (.env)
-    omitNull: true      // solo Postgres
+    storage: storage, // solo SQLite (.env)
+    omitNull: true // solo Postgres
 });
 
 // Importar la definicion de la tabla Quiz en quiz.js
@@ -33,16 +33,20 @@ exports.Quiz = Quiz;
 
 // Sequielize.sync() crea e inicializa la tabla de preguntas en DB
 sequelize.sync().success(function () {
-            // succes(...) ejecuta el manejador una vez creada la tabla
-            Quiz.count().success(function (count) {
-                    if (count === 0) { // La tabla solo se inicializa si esta vacia
-                        Quiz.create({
-                                pregunta: 'Capital de Italia',
-                                respuesta: 'Roma'
-                            })
-                            .success(function () {
-                                console.log('Base de Datos inicializada')
-                            });
-                    };
+    // succes(...) ejecuta el manejador una vez creada la tabla
+    Quiz.count().success(function (count) {
+        if (count === 0) { // La tabla solo se inicializa si esta vacia
+            Quiz.create({
+                pregunta: 'Capital de Italia',
+                respuesta: 'Roma'
+            });
+            Quiz.create({
+                    pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
+                })
+                .then(function () {
+                    console.log('Base de Datos inicializada')
                 });
+        };
+    });
 });
