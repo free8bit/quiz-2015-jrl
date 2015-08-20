f599867bc78e3561278c423deef56e8930cb7d94
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2015-08-19 21:28:27 -0300

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,9 @@ router.get('/', function (req, res) {
     });
 });
 
-// Autoload de comandos con :quizId
+// Autoload de comandos con :quizId y :commentId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definicion de rutas de session
 router.get('/login', sessionController.new); // formulario login
@@ -31,8 +32,10 @@ router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizCo
 router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
+// Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function (req, res) {
     res.render('author', {
